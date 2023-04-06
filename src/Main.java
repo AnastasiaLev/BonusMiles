cea38276b207e43e94763bbe37f1b83aed94ffc9
Neto test <alevochkina96@gmail.com>
2023-04-06 19:51:22 +0700

    bonus

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        int i = 2280;
+        int x = 20;
+        int b = i / x;
+        System.out.println(b);
+
+    }
+}
