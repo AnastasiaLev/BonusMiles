b7b65b71d1600de1156d2ae580a83b495bac53d2
Neto test <alevochkina96@gmail.com>
2023-04-09 13:43:37 +0700

    bonus

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        int TicketPrice = 2280;
+        int ticketPrice = 2280;
         int rub = 20;
-        int miles = TicketPrice / rub;
+        int miles = ticketPrice / rub;
         System.out.println(miles);
 
     }
